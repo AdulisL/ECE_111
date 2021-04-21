fff94be8d7e543f621670b7d157653bf0bfb323a
Adulis <named.adulis@gmail.com>
2021-04-21 10:03:39 -0700

    Inverted ring_counter

--- Homework3/johnson_counter.sv
+++ Homework3/johnson_counter.sv
@@ -1,35 +1,34 @@
 module johnson_counter #(parameter WIDTH = 4)(
     input logic clk, clear, preset,
-    logic [WIDTH-1:0] load_cnt = 4'b1000,
-    output wire[WIDTH-1:0] count
+    logic [WIDTH-1:0] load_cnt,
+    output logic[WIDTH-1:0] count
 );
-    reg [WIDTH-1:0] reg_count;
+    // reg [WIDTH-1:0] reg_count;
 
-    initial
-    begin
-        reg_count = 4'b0000;
-        $stop;
-    end
+    // initial
+    // begin
+    //     reg_count = 4'b0000;
+    //     $stop;
+    // end
 
-    always @(posedge clk, negedge clear) begin //clk rising and falling edges
+    always @(posedge clk or negedge clear) begin //clk rising and falling edges
         if(!clear) begin
-            reg_count <= 0;            // clear j_counter
+            // reg_count = 0;            // clear j_counter
+            count <= 4'b0000;
         end
         else if(!preset)begin
-            reg_count <= load_cnt;     // load load_count
+            // reg_count = load_cnt;     // load load_count
+            count <= load_cnt;
         end
         else begin
-            reg_count <= reg_count + 1;   
+
+            // reg_count[2:0] = reg_count[3:1];  // right-shift
+            count    <= count >> 1;
+            count[3] <= ~count[0];
+
         end
     end
-     // Johnson right shift counter
-    always @(posedge clk) begin
-        reg_count[2:0] = reg_count[3:1];  // right-shift fill 0 on the MSB
-        reg_count[3] = !reg_count[0];     //Circulate inverted LSB to MSB
-        // reg_count <= reg_count >> 1;   // right-shift operator
-    end
-
     // j_Counter value assigned to output port count  
-    assign count = reg_count;
+    // assign count = reg_count;
    
 endmodule: johnson_counter  // Module end declaration
