547d80694ec26879393ba73ad5b874cfbcb3c654
Adulis <named.adulis@gmail.com>
2021-05-10 14:28:25 -0700

    function call updated

--- Homework6/Lab6/gray_code_to_binary_convertor/gray_code_to_binary_convertor.sv
+++ Homework6/Lab6/gray_code_to_binary_convertor/gray_code_to_binary_convertor.sv
@@ -4,21 +4,22 @@ module gray_code_to_binary_convertor #(parameter N = 4)(
   output logic[N-1:0] binary_value);
  
   // Add code for gray code to binary conversion
- 
-  /* gray_code convertion using funtion call */
-  always_ff @(posedge ck or negedge arstn) 
+
+ /* gray_code convertion using funtion call */
+  always_ff @(posedge clk or negedge rstn) 
   begin
-    if(!reset) binary_value <= 0;
+    if(!rstn) binary_value <= 0;
     else binary_value <= gray_to_binary(gray_value);
   end
-  
+
   // gray_code to binary_value function 
-  function automatic gray_to_binary(logic[N-1:0] value);
+  function automatic[N-1:0] gray_to_binary(logic[N-1:0] value);
     begin
-      binary_value[N-1] = value[N-1];
-      foreach (value[i])  // iterate through every element in the array
-        binary_value[i-1] = value[i]^value[i-1];  
-    end  
-    
+      gray_to_binary[N-1] = value[N-1];
+      // iterate through every element in the array
+      foreach (value[i])  
+        gray_to_binary[i-1] = value[i]^value[i-1];  
+    end   
   endfunction 
+  
 endmodule: gray_code_to_binary_convertor
